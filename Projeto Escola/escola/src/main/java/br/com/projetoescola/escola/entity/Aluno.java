6ec6fa913d48b39726b00d8e389d7be99c91cd31
Leon <leonhenrique423@gmail.com>
2025-08-18 20:47:16 -0300

    tabela e bd

--- Projeto Escola/escola/src/main/java/br/com/projetoescola/escola/entity/Aluno.java
+++ Projeto Escola/escola/src/main/java/br/com/projetoescola/escola/entity/Aluno.java
@@ -1,15 +1,39 @@
 package br.com.projetoescola.escola.entity;
 
-@entity
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Aluno {
     
-    @id
-    @GeneratedValue(strategy = Generation)
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer idAluno;
+
+    @Column (nullable = false, length = 40)
     private String nomeAluno;
+
+    @Column (nullable = false, length = 11)
     private String telefoneAluno;
-    private String idAluno;
+
+    @Column (nullable = false, length = 40)
     private String enderecoAluno;
+
+    @Column (nullable = false, length = 11)
     private String rmAluno;
+
+    @Column (nullable = false, length = 40)
     private String cpfAluno;
+    
 }
